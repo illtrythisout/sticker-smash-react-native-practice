2cf2c24ed9ec23b9c789ea1bacad287d97151659
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-12-08 17:34:09 +0100

    Implement image picking functionality

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,21 +1,43 @@
-//  Packages
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
-// Components
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
 
-// Assets
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
-      <View>
-        <Button theme="primary" label="Choose a photo" />
+      <View style={styles.footerContainer}>
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
     </View>
@@ -32,7 +54,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   footerContainer: {
-    flex: '1 / 3',
+    flex: 1 / 3,
     alignItems: 'center',
   },
 });
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,13 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 type Props = {
   label: string;
   theme?: 'primary';
-}
+  onPress?: () => void;
+};
 
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
   if (theme === 'primary') {
     return (
       <View
@@ -14,22 +15,21 @@ export default function Button({ label, theme }: Props) {
           styles.buttonContainer,
           { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
         ]}>
-        <Pressable
-          style={[styles.button, { backgroundColor: '#fff' }]}
-          onPress={() => alert('You pressed a button.')}>
+        <Pressable style={[styles.button, { backgroundColor: '#fff' }]} onPress={onPress}>
           <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
         </Pressable>
       </View>
     );
   }
+
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button')}>
+      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -55,5 +55,5 @@ const styles = StyleSheet.create({
   buttonLabel: {
     color: '#fff',
     fontSize: 16,
-  }
-})
+  },
+});
